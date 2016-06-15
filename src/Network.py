5826028eb5460a7ceacb7eefead1933dcb757dd1
Josh Shifman <shifmanj@umich.edu>
2016-06-15 12:36:12 -0400

    initial commit, sigmoid and init functions

---
+++ src/Network.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def sigmoid(z):
+	return 1.0/(1.0 + np.exp(-z))
+
+class Network(object):
+
+	def __init__(self, sizes):
+		self.num_layers = len(sizes)
+		self.sizes = sizes
+		# note: sizes[1:] --> we don't add biases for our input layer
+		self.biases = [np.random.randn(y,1) for y in sizes[1:]]
+		self.weights = [np.random.randn(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
+
