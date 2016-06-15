9634f3f1d40182fae782575e427a6bac5fadfb56
Josh Shifman <shifmanj@umich.edu>
2016-06-15 10:59:25 -0400

    add feedForward and activate functions

--- src/Perceptron.py
+++ src/Perceptron.py
@@ -8,8 +8,16 @@ class Perceptron:
 			#initialize n random weights
 			self.weights.append(random.uniform(-1, 1))
 
-	def feedForward(inputs):
+	def feedForward(self, inputs):
 		sum = 0
-		for i in range(0, len(self.weights):
+		for i in range(0, len(self.weights)):
 			sum += inputs[i] * self.weights[i]
+
+		return self.activate(sum)
+
+
+	# This function ought to take another function as input?
+	def activate(self, sum):
+		# Sign Function
+		return 1 if sum > 0 else -1
 
