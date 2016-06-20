f0d9d9a78a30903d618b1fa7e33aab259d0596b7
Josh Shifman <shifmanj@umich.edu>
2016-06-20 15:36:17 -0400

    update tests

--- src/test.py
+++ src/test.py
@@ -1,7 +1,12 @@
-from Perceptron import Perceptron
+import mnist_loader
+import Network
 
 
 def main():
 	
+	train, validation, test = mnist_loader.load_data_wrapper()
+	net = Network.Network([784, 30, 10])
+	net.SGD(train, 30, 10, 3.0, test_data=test)
+
 if __name__ == '__main__':
 	main()
